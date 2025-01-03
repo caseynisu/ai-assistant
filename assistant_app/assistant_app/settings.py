83810d9a26ff1708cd4df31463ac9e0f4e4ce52c
Nisu <caseynishu@gmail.com>
2025-01-04 02:25:06 +0545

    New changes

--- assistant_app/assistant_app/settings.py
+++ assistant_app/assistant_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h7#^vmcvmev()l#&_l*%flp+0)%r_2d-zy3t-b3p5jv!+!f#-2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
